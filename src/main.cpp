bae0b663e093ccb1e4693ec66d09d2749563ad2f
danielbernalb <danielbernalb@gmail.com>
2020-11-16 10:49:25 -0500

    New boards supported: ESP32S and ESP32DevKit

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ void startingSensors() {
     sensors.setOnDataCallBack(&onSensorDataOk);   // all data read callback
     sensors.setSampleTime(cfg.stime);             // config sensors sample time
     sensors.setDebugMode(false);                  // [optional] debug mode
-    sensors.init(cfg.getSensorType());            // start all sensors and
+    sensors.init(sensors.Auto);                  // start all sensors and
                                                   // try to detect configured PM sensor.
                                                   // Sensors supported: Panasonic, Honeywell, Plantower and Sensirion
                                                   // The configured sensor is choosed on Android app.
